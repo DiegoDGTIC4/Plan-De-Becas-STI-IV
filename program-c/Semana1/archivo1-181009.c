53262117de846ad60fb7ece6bbb434e2d1b4f225
Diego Mora Monroy <fedo09440@gmail.com>
2018-10-09 14:02:04 -0700

    Primero Programa

---
+++ program-c/Semana1/archivo1-181009.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+int Numero;
+Numero = 1;
+
+printf("Hola diego\n");
+
+printf("Hola diego\t");
+
+
+printf("\nMi numero es %d",Numero);
+
+Numero = 3;
+
+printf("\nMi numero es %d",Numero);
+
+
+}
