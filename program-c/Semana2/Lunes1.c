46fda571bc0104848ebd263203ed59d77b3b4f0e
Diego Mora Monroy <fedo09440@gmail.com>
2018-10-15 22:09:38 -0500

    Estos son mis primeros programas en c

---
+++ program-c/Semana2/Lunes1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+
+int numero;
+int contador = 1;
+
+
+printf("Por favor ingrese valor: ");
+scanf(" %d",&numero);
+
+while (contador <= numero) {
+
+
+
+printf("%d\n", contador);
+contador = contador + 1;
+
+
+}
+
+}
+
