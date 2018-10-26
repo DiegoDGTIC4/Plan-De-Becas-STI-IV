8c6398e30a2eed1236275b466f7274103030bf38
Diego Mora Monroy <fedo09440@gmail.com>
2018-10-26 01:54:06 -0500

    Programar es difícil

---
+++ program-c/TAREAS/Tarea_14.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+
+
+int main ()
+
+{
+
+
+int convertidor;
+int binario;
+int octal;
+int hexadecimal;
+
+printf("Este programa te calcula de entero a binario, octal y hexadecimal\n");
+
+printf("Seleccione que convertidor quiere usar\n");
+
+printf("1 - Decimal a Binario\n");
+printf("2 - Decimal a Octal\n");
+printf("3 - Decimal a Hexadecimal\n");
+scanf(" %d", &convertidor);
+
+
+
+switch(convertidor){
+
+
+	case 1:
+
+	do{
+
+	printf("Ingrese un numero porfavor: ");
+	scanf(" %d", &binario);
+	
+	}
+
+
+	while (binario < 0);
+	break;
+
+
+
+
+	case 2 :
+
+	 printf("Ingrese un numero porfavor: ");
+        scanf(" %d", &octal);
+
+	printf("Su numero en Octal es: %o\n", octal);
+	break;
+
+
+	case 3 :
+
+         printf("Ingrese un numero porfavor: ");
+        scanf(" %d", &hexadecimal);
+
+        printf("Su numero en Hexadecimal es: %x\n", hexadecimal);
+        break;
+
+default:
+
+printf("Este programa no realiza lo que usted quiere\n");
+
+}
+
+}
