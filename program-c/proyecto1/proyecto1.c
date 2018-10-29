f3c101afb337a4a4893b3c21fe2552c7a5d4efd8
Diego Mora Monroy <fedo09440@gmail.com>
2018-10-28 23:50:18 -0600

    Sigo programando

--- program-c/proyecto1/proyecto1.c
+++ program-c/proyecto1/proyecto1.c
@@ -1,29 +1,31 @@
 #include<stdio.h>
 
 
-int main(){
+int main()
+{
 
-	char matriz[2][3] = {{'a','b','c'},{'d','e','f'}};
+char ma[4][13]={{'q','w','e','r','t','y','u','i','o','p','a','s','d'},{'f','g','h','j','k','l','z','x','c','v','b','n','m'},{'n','m','b','v','c','x','z','l','k','j','h','g','f'},{'d','s','a','p','o','i','u','y','t','r','e','w','q'}};
+	int mi[4]={1,2,3,4};
+	printf("**********************************************\n");
+	printf("*Bienvenido a esta maravillosa sopa de letras*\n");
+	printf("**********************************************\n");
+	int j,i,k,m=1;
+	printf("1  2  3  4  5  6  7  8  9 10 11 12 13\n"); 
+	for(i=0;i<4;i++){
 
-	int i;
-	int j;
-		
-		for ( i = 0; i < 2; i++){
-
-			for ( j = 0; j < 3; j++){
-
-
-
-
-	printf("%c ", matriz[i][j]);
-
-}
-
-
-	printf("\n");
+	for(j=0;j<13;j++){
+			
+			printf("%c  ",ma[i][j]);
+			
+		}
+		printf(" %d",m++);
+		printf("\n");
+	}
+	printf("Instruciones:Encuentra todos los pares de las letras presentadas en sopa\nEjemplo:\na=3,4 11,1\n");
 
 
 
-}
+
+
 
 }
